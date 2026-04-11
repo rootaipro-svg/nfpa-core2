1583ecbbf6fc8e20dea71d46be8352cb07514afa
jod <rootaipro@gmail.com>
2026-04-11 13:43:49 +0300

    Delete lib/supabase.ts

--- lib/supabase.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-// وضعنا المفاتيح هنا مباشرة لنتجاوز مشاكل إعدادات Vercel نهائياً
-const supabaseUrl = 'https://uysfhchahbayozbisppy.supabase.co';
-const supabaseKey = 'sb_publishable_T03nYMwpGp1uXXTPLqx_1Q_JnzMuqML';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
